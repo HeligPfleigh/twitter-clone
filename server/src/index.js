b5c78409308425146f058e96e965173996d3afe3
tuan.tran <tuan.tran@mttjsc.com>
2018-03-09 15:23:25 +0700

    Add faker library to fake data to our database

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ import './config/db';
 import constants from './config/constants';
 import typeDefs from './graphql/schema';
 import resolvers from './graphql/resolvers';
+import mocks from './mocks';
 
 const app = express();
 
@@ -25,10 +26,13 @@ app.use(constants.GRAPHQL_PATH, graphqlExpress({
   schema,
 }));
 
-app.listen(constants.PORT, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`App listen on port: ${constants.PORT}`);
-  }
+mocks().then(() => {
+  app.listen(constants.PORT, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`App listen on port: ${constants.PORT}`);
+    }
+  });
 });
+
