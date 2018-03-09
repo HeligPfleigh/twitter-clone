195c7468de7cc32ca41e18c714031e6afd8b9d56
tuan.tran <tuan.tran@mttjsc.com>
2018-03-09 17:20:43 +0700

    Add typedefs for User in schema.js

--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -12,6 +12,17 @@ export default `
     updatedAt: Date!
   }
 
+  type User {
+    _id: ID!
+    username: String
+    email: String!
+    firstName: String
+    lastName: String
+    avatar: String
+    createdAt: Date!
+    updatedAt: Date!
+  }
+
   type Query {
     getTweet(_id: ID!): Tweet
     getTweets: [Tweet]
