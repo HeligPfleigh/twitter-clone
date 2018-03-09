8b254885b976f1adb5c728b0a623061b3ce9b8ea
tuan.tran <tuan.tran@mttjsc.com>
2018-03-09 11:05:52 +0700

    Add nodemon and first initialize for server

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`App listen on port: ${PORT}`);
+  }
+});
