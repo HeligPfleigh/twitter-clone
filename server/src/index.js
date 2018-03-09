2c0d9c3bc8397fd24ff4cf1c5677686593b80d32
tuan.tran <tuan.tran@mttjsc.com>
2018-03-09 14:58:46 +0700

    Config graphql apollo for server

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,34 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 
 import './config/db';
+import constants from './config/constants';
+import typeDefs from './graphql/schema';
+import resolvers from './graphql/resolvers';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 app.use(bodyParser.json());
 
-app.listen(PORT, (err) => {
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: constants.GRAPHQL_PATH,
+}));
+
+app.use(constants.GRAPHQL_PATH, graphqlExpress({
+  schema,
+}));
+
+app.listen(constants.PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`App listen on port: ${PORT}`);
+    console.log(`App listen on port: ${constants.PORT}`);
   }
 });
