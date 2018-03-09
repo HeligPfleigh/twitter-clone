02a6543a074a0c1373151ee52db985198cd2b48c
tuan.tran <tuan.tran@mttjsc.com>
2018-03-09 16:54:31 +0700

    Add date scalar for graphql

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,6 +1,9 @@
+import GraphQlDate from 'graphql-date';
+
 import TweetResolvers from './tweet-resolvers';
 
 export default {
+  Date: GraphQlDate,
   Query: {
     getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets,
