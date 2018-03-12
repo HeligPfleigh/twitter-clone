5d4cd9457835c23d73ebbab5a56d590c741ad286
tuan.tran <tuan.tran@mttjsc.com>
2018-03-12 16:31:00 +0700

    Add missing

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -2,9 +2,13 @@ import GraphQlDate from 'graphql-date';
 
 import TweetResolvers from './tweet-resolvers';
 import UserResolvers from './user-resolvers';
+import User from '../../models/User';
 
 export default {
   Date: GraphQlDate,
+  Tweet: {
+    user: ({ user }) => User.findById(user),
+  },
   Query: {
     getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets,
